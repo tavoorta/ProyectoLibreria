f2c1efa63fe5b000fcf59f7267e4cafde2a7f766
Gustavo Romero <ggaro150882@gmail.com>
2024-08-18 17:56:50 -0500

    first commit

---
+++ src/main/java/app/Conexion.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ggaro
+ */
+public class Conexion {
+    public static Connection getConnections(){
+        Connection connection = null;
+        // definir datos para crear conexion
+        var database = "escuela";
+        var url = "jdbc:mysql://localhost:3306/" + database;
+        var username = "root";
+        var password = "753159";
+
+        // Cargar clase del driver mysql en memoria
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        //catch(Exception e){
+        catch(ClassNotFoundException | SQLException e){
+            System.out.println("Error en conexion a BD: " + e.getMessage());
+        }
+
+        return connection;
+    }
+}
